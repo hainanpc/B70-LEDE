0fcc8b09998f6fd1938d773d6ef1e6913c62b401
Liangliang <57309217+hainanpc@users.noreply.github.com>
2021-02-09 08:54:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,4 @@
 
 # Lienol/openwrt-package
 # sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
